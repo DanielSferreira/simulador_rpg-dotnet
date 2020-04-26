4e8c13dc70d42134d6d41c7ce0ceeb6a5edbfb4f
Daniel Ferreira <daniel6991ferreira@gmail.com>
2020-04-26 12:45:26 -0300

    primeiro commit do projeto

---
+++ simulador-rpg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simulador_rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Agora vamos colocar a mão na massa!");
+        }
+    }
+}
